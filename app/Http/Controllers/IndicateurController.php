bf1c8f04b0055599f0c4aeb46647af0e51f3da65
Matthias.D <matth4627@gmail.com>
2025-02-25 16:58:15 +0100

    Affichage des indicateurs uniquement lié a l'utilisateur connecté

--- app/Http/Controllers/IndicateurController.php
+++ app/Http/Controllers/IndicateurController.php
@@ -4,13 +4,14 @@ namespace App\Http\Controllers;
 
 use App\Models\Indicateur;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class IndicateurController extends Controller
 {
     public function index()
     {
         // Récupérer tous les utilisateurs de la base de données
-        $indicateurs = Indicateur::all();
+        $indicateurs = Auth::user()->indicateurs;
 
         // Passer les utilisateurs à la vue
         return view('indicateur', compact('indicateurs')); // Remplacez 'votre_vue' par le nom de votre fichier blade.
---
+++ app/Models/IndicateurUser.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class IndicateurUser extends Model
+{
+    use HasFactory;
+
+    protected $table = 'indicateur_user'; // Spécifiez le nom de la table
+}
