5af7c0e9c73592f8f97198e75805b0114e668f7c
Matthias.D <matth4627@gmail.com>
2025-02-25 12:27:15 +0100

    Changement du header

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -261,13 +261,45 @@
             <nav class="flex items-center justify-end gap-4">
                 @auth
                     <a href="{{ url('/dashboard') }}"
-                       class="inline-block px-5 py-1.5 border-[#19140035] hover:border-[#1915014a] hover:bg-[#19140035] hover:font-medium border text-[#354f44] rounded-sm text-sm leading-normal transition-colors duration-200"
+                       class="inline-block px-5 py-1.5 border-[#19140035] hover:border-[#1915014a] hover:bg-[#19140035] hover:font-medium border text-[#354f44] rounded-md text-sm leading-normal transition-colors duration-200"
                        aria-label="Tableau de bord">
                         Indicateurs
                     </a>
+
+                    <div class="hidden sm:flex sm:items-center sm:ms-6">
+                        <x-dropdown align="right" width="48">
+                            <x-slot name="trigger">
+                                <button class="inline-block px-5 py-1.5 border-[#19140035] hover:border-[#1915014a] hover:bg-[#19140035] hover:font-medium border text-[#354f44] rounded-md text-sm leading-normal transition-colors duration-200">
+                                    <div>{{ Auth::user()->name }}</div>
+
+                                    <div class="ms-1">
+                                        <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                            <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                                        </svg>
+                                    </div>
+                                </button>
+                            </x-slot>
+
+                            <x-slot name="content">
+                                <x-dropdown-link :href="route('profile.edit')" class="block w-full px-5 py-1.5 border-none text-left text-[#354f44] hover:bg-[#19140035] hover:text-white rounded-md text-sm leading-normal transition-colors duration-200">
+                                    {{ __('Profile') }}
+                                </x-dropdown-link>
+
+                                <form method="POST" action="{{ route('logout') }}">
+                                    @csrf
+
+                                    <x-dropdown-link :href="route('logout')"
+                                                     onclick="event.preventDefault();
+                this.closest('form').submit();" class="block w-full px-5 py-1.5 border-none text-left text-[#354f44] hover:bg-[#19140035] hover:text-white rounded-md text-sm leading-normal transition-colors duration-200">
+                                        {{ __('Log Out') }}
+                                    </x-dropdown-link>
+                                </form>
+                            </x-slot>
+                        </x-dropdown>
+                    </div>
                 @else
                     <a href="{{ route('login') }}"
-                       class="inline-block px-5 py-1.5 text-[#354f44] border border-transparent hover:bg-[#19140035] hover:border-[#19140035] hover:font-medium rounded-sm text-sm leading-normal transition-colors duration-200"
+                       class="inline-block px-5 py-1.5 border-[#19140035] hover:border-[#1915014a] hover:bg-[#19140035] hover:font-medium border text-[#354f44] rounded-md text-sm leading-normal transition-colors duration-200"
                        aria-label="Se connecter">
                         Se connecter
                     </a>
