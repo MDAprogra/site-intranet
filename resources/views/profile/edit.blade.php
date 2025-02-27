3a816d899ff14dde0fc7992a7474ffbbf5796b28
Matthias.D <matth4627@gmail.com>
2025-02-27 11:32:43 +0100

    Modif de la partie info profil et mise en commentaire

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Profile') }}
+        <h2 class="font-semibold text-xl text-gray-200 leading-tight">
+            {{ __('Profil') }}
         </h2>
     </x-slot>
 
@@ -19,11 +19,13 @@
                 </div>
             </div>
 
-            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    @include('profile.partials.delete-user-form')
-                </div>
-            </div>
+{{--            27/02/2025 -- Mis en commentaire pour empêcher les utilisateurs de supprimer leur compte--}}
+
+{{--            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">--}}
+{{--                <div class="max-w-xl">--}}
+{{--                    @include('profile.partials.delete-user-form')--}}
+{{--                </div>--}}
+{{--            </div>--}}
         </div>
     </div>
 </x-app-layout>
