a92842fb00e5fd93fa7ecd641cb8547a7654f7fe
Matthias.D <matth4627@gmail.com>
2025-02-27 11:01:08 +0100

    Amélioration de la page d'accueil welcome.blade.php ----> accueil.blade.php

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,7 @@ use App\Http\Controllers\User\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('accueil');
 })->name('welcome');
 
 Route::get('/dashboard', function () {
