16d8684b957ce6b4a8a372ccf8dfe06754d92ad5
Matthias.D <matth4627@gmail.com>
2025-03-11 11:21:26 +0100

    Suppr code inutile

--- app/Http/Controllers/Indicateurs/DevisController.php
+++ app/Http/Controllers/Indicateurs/DevisController.php
@@ -10,19 +10,6 @@ class DevisController extends Controller
 {
     public function index()
     {
-//        $DevisSemaine = DB::connection('pgsql')->select("SELECT
-//            TO_CHAR(endv_date, 'YYYY-WW') AS semaine,
-//            COUNT(*) AS nombre
-//        FROM
-//            fd_entete_devi
-//        WHERE
-//            TO_CHAR(endv_date, 'YYYY') >= '2024'
-//            AND endv_init_dev IN ('CCE', 'DBD', 'DGY', 'IAE', 'KPN', 'QRN', 'TSE')
-//        GROUP BY
-//            TO_CHAR(endv_date, 'YYYY-WW')
-//        ORDER BY
-//            semaine DESC;");
-
         $Devis = DB::connection('pgsql')->select("SELECT endv_date,
        COUNT(*) AS nombre
 FROM fd_entete_devi
