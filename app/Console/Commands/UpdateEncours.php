db736625e681dc510acd777a3ae0a52a26b05e32
Matthias.D <matth4627@gmail.com>
2025-05-07 16:41:06 +0200

    Add UpdateEncours command for FTP export of encours

---
+++ app/Console/Commands/UpdateEncours.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Tools\AccessoiresFTP;
+use Illuminate\Console\Command;
+
+class UpdateEncours extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:update-encours';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $start = microtime(true);
+        $access_ftp = new AccessoiresFTP();
+        try {
+
+
+            $access_ftp->getFTP('Imports_Automatiques/GAMSYS/', 'encours Export.txt', '/mnt/interfas/DEV/YB_linux/Yellowbox/');
+        } catch (\Throwable $e) {
+            $this->error('Erreur lors de l\'exportation des encours : ' . $e->getMessage());
+            return Command::FAILURE;
+        }
+        $this->info('Fichier encours Export.txt téléchargé avec succès.');
+        $duration = microtime(true) - $start;
+        $this->info('Durée d\'exécution : ' . round($duration, 2) . ' secondes');
+        return Command::SUCCESS;
+
+    }
+}
