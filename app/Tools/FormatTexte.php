173ed6092b873c4c9135a166c4ed45a62dc26b04
Matthias.D <matth4627@gmail.com>
2025-05-07 10:41:42 +0200

    Update deployment configuration and modify file handling in FormatTexte

--- app/Tools/FormatTexte.php
+++ app/Tools/FormatTexte.php
@@ -32,31 +32,48 @@ class FormatTexte
 
     public function YBcreateFileTXT($FileName, $FilePath, $FileData)
     {
-        try {
-            $file = fopen($FilePath . $FileName, 'w+');
-        }
-        catch (\Exception $e) {
-            throw new \Exception("Le fichier n'a pas pu être créé : " . $e->getMessage());
-        }
+        // ouverture du fichier par le disque YellowBox
+        $fullPath = $FilePath . $FileName;
+
+        dd($fullPath);
+
+        $file = fopen($fullPath, 'w+');
 
         if ($file === false) {
             throw new \Exception("Le fichier n'a pas pu être ouvert.");
         }
 
-        fwrite($file, "\xEF\xBB\xBF"); // Ajouter le BOM UTF-8 (Byte Order Mark)
-
-        $enTete = (array)$FileData[0];
-        $header = implode(';', array_keys($enTete));
-        fwrite($file, $header . PHP_EOL);
-
-        foreach ($FileData as $row) {
-            $row = (array)$row;
-            $line = implode(';', array_map(function ($value) {
-                return '"' . str_replace('"', '""', $value) . '"';
-            }, $row));
-            fwrite($file, $line . PHP_EOL);
-        }
-
         fclose($file);
+
+
+
+
+
+//        try {
+//            $file = fopen($FilePath . $FileName, 'w+');
+//        }
+//        catch (\Exception $e) {
+//            throw new \Exception("Le fichier n'a pas pu être créé : " . $e->getMessage());
+//        }
+//
+//        if ($file === false) {
+//            throw new \Exception("Le fichier n'a pas pu être ouvert.");
+//        }
+//
+//        fwrite($file, "\xEF\xBB\xBF"); // Ajouter le BOM UTF-8 (Byte Order Mark)
+//
+//        $enTete = (array)$FileData[0];
+//        $header = implode(';', array_keys($enTete));
+//        fwrite($file, $header . PHP_EOL);
+//
+//        foreach ($FileData as $row) {
+//            $row = (array)$row;
+//            $line = implode(';', array_map(function ($value) {
+//                return '"' . str_replace('"', '""', $value) . '"';
+//            }, $row));
+//            fwrite($file, $line . PHP_EOL);
+//        }
+//
+//        fclose($file);
     }
 }
