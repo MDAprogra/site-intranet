d3336b179fd285aa0e5e2ace1b1b1836edd603e3
Matthias.D <matth4627@gmail.com>
2025-05-07 14:44:55 +0200

    Refactor SQL query formatting in AMP.sql and update dialect mapping in sqldialects.xml

--- database/sql/AMP.sql
+++ database/sql/AMP.sql
@@ -1,8 +1,15 @@
-SELECT OPRE_SAL, OPRE_POSTE, OPRE_DATE,
-       to_char(OPRE_H_DEBUT, 'HH24:MI') as \"Heure départ\",
-to_char(OPRE_H_FIN, 'HH24:MI')   as \"Heure fin\",
-OPRE_DUREE, OPRE_TAUX_1, OPRE_TAUX_2, OPRE_QUANTITE, OPRE_CODE_OP, OPRE_LIBELLE_OPE
-            FROM FP_OPERA_REEL
-            WHERE OPRE_DOSSIER = ''
-            AND OPRE_DATE = ?
-            AND OPRE_LIBELLE_OPE = 'Attente Matière Première';
+SELECT OPRE_SAL,
+       OPRE_POSTE,
+       OPRE_DATE,
+       to_char(OPRE_H_DEBUT, 'HH24:MI') as "Heure départ",
+       to_char(OPRE_H_FIN, 'HH24:MI')   as "Heure fin",
+       OPRE_DUREE,
+       OPRE_TAUX_1,
+       OPRE_TAUX_2,
+       OPRE_QUANTITE,
+       OPRE_CODE_OP,
+       OPRE_LIBELLE_OPE
+FROM FP_OPERA_REEL
+WHERE OPRE_DOSSIER = ''
+  AND OPRE_DATE = ?
+  AND OPRE_LIBELLE_OPE = 'Attente Matière Première';
